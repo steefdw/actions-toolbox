f68f7f65e786aaf2cdc270f0ee53ec89e751632f
Steef de Winter <steef@numarketing.nl>
2025-11-01 18:00:53 +0100

    added build script that creates a `mise.toml` config file and builds the docker container with mise installed

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+foo="$2"
+input="$1"
+
+echo "foo = $foo"
+
+echo "[tools]" > mise.toml
+
+echo "$input" | while IFS= read -r line; do
+  [[ -z "$line" ]] && continue
+
+  tool=$(echo "$line" | cut -d':' -f1 | xargs)
+  version=$(echo "$line" | cut -d':' -f2- | xargs)
+  echo "${tool} = \"${version}\"" >> mise.toml
+done
+
+[ ! -d ./mise ] && mkdir mise && echo "created mise folder" || echo "mise folder found"
+
+docker build -t actions-tookbox .
+docker run -it --rm -v ./mise:/mise -v ./mise.toml:/mise.toml actions-tookbox mise install
