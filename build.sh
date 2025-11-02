eb98b6328645c332f30485923d20d962faec69dc
Steef de Winter <steef@numarketing.nl>
2025-11-02 13:21:55 +0100

    debug

--- build.sh
+++ build.sh
@@ -1,21 +1,43 @@
 #!/bin/bash
 
+actionPath="$( cd "$(dirname "$0")" || exit >/dev/null 2>&1 ; pwd -P )"
+readonly actionPath
+
+repositoryPath="$PWD"
+readonly repositoryPath
+
 foo="$2"
 input="$1"
 
 echo "foo = $foo"
 
-echo "[tools]" > mise.toml
+echo "[tools]" > ./mise.toml
 
 echo "$input" | while IFS= read -r line; do
   [[ -z "$line" ]] && continue
 
   tool=$(echo "$line" | cut -d':' -f1 | xargs)
   version=$(echo "$line" | cut -d':' -f2- | xargs)
-  echo "${tool} = \"${version}\"" >> mise.toml
+  echo "${tool} = \"${version}\"" >> ./mise.toml
 done
 
-[ ! -d ./mise ] && mkdir mise && echo "created mise folder" || echo "mise folder found"
+cat ./mise.toml
 
-docker build -t actions-tookbox -f ./build/Dockerfile .
-docker run -it --rm -v ./mise:/mise -v ./mise.toml:/mise.toml actions-tookbox mise install
+[ ! -d ./mise ] \
+  && mkdir mise && echo "created mise folder in ${repositoryPath}" \
+  || echo "mise folder found in ${repositoryPath}"
+
+ls -lah
+ls -lah "$actionPath"/build || echo "no build folder found in ${actionPath}"
+
+tagName='actions-toolbox:latest'
+if ! docker build -t "$tagName" -f "$actionPath"/build/Dockerfile "$actionPath"
+then
+  echo "Docker build failed"
+  exit 1
+fi
+
+docker run --rm \
+  -v "${repositoryPath}/mise:/mise" \
+  -v "${repositoryPath}/mise.toml:/mise.toml" \
+  "$tagName" mise install
